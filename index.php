213820552aed9e9ee9bb5771a1e46cb1f6e98331
douglas <douglascastrorj@gmail.com>
2015-11-25 16:58:33 -0200

    first

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+	<head>
+		<title>Sistema Oficina</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	</head>
+	<body>
+
+		<nav class="navbar navbar-dark bg-primary">
+			<a class="navbar-brand" href="#">Navbar</a>
+
+			<ul class="nav navbar-nav">
+			    <li class="nav-item active">
+			      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			    </li>
+			    <li class="nav-item">
+			      <a class="nav-link" href="#">Features</a>
+			    </li>
+			    <li class="nav-item">
+			      <a class="nav-link" href="#">Pricing</a>
+			    </li>
+			    <li class="nav-item">
+			      <a class="nav-link" href="#">About</a>
+			    </li>
+			  </ul>
+			  
+			  <form class="form-inline navbar-form pull-right">
+			    <input class="form-control" type="text" placeholder="Search">
+			    <button class="btn btn-success-outline" type="submit">Search</button>
+			  </form>
+		</nav>
+
+		<div class="container-fluid">
+		  <div class="row">
+		    <div class="col-md-4" style="background-color:#eee">
+		      One of three columns
+		    </div>
+		    <div class="col-md-8">
+		      One of three columns
+		    </div>
+		    
+		  </div>
+		</div>
+		
+	</body>
+</html>
